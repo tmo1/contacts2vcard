e522217a2c0a2d5581090eea1d1ed06fbd30de6e
Thomas More <tmore1@gmx.com>
2020-04-20 14:25:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+contacts2vcard
+==============
+contacts2vcard is a Perl script for converting Android contacts to vCard format. Its motivation was the need to extract contacts from a backup copy (produced by Oandbackup) of the Android contacts database and import them into a current Android installation. It has been designed for and tested on a copy of the contacts database ('contacts2.db') from Android 9 (Pie) (LineageOS); I do not know how well it will work, if at all, on other versions. The code is based on the excellent (unofficial) documentation of the database format here:
+ * https://www.dev2qa.com/android-contacts-database-structure/
+ * https://www.dev2qa.com/android-contacts-fields-data-table-columns-and-data-mimetype-explain/
+
+Currently, only name, phone number, and email data are extracted. A future version of the script may extract other data as well.
+
+Dependencies
+------------
+The script depends on the Perl modules DBD::SQLite and vCard (Debian: libdbd-sqlite3-perl and libtext-vcard-perl).
+
+Invocation
+----------
+The script is invoked as `contacts2vcard -d database -a address_book`, where 'database' is the database file (including path - the default is 'contacts2.db' in the current directory), and 'address_book' is the name of a file (including path - the default is 'address_book' in the current directory) to output the vCard address book to. The error `DBD::SQLite::db selectall_arrayref failed: no such table: data at perl/contacts2vcard.pl line 39.` indicates that the script has been pointed at an invalid file or (likely) a nonexistent one.
+
+Alternatives
+------------
+There are a number of other scripts that purport to do what contacts2vcard does; I have not tried them or studied their code, and contacts2vcard is not based on their code:
+ * https://thydzik.com/export-android-contacts-contacts2-db-to-vcard-vcf-on-windows/
+ * https://github.com/stachre/dump-contacts2db
+ * https://gist.github.com/fenrir-naru/9dafa69e51c7ea99e1ccf5a863da728f
+ * https://gist.github.com/1d10t/8ef7a55b5d6ab74b8300ea61376bb678
+
+Cf.:
+ * https://stackoverflow.com/questions/18955512/converting-db-to-vcf
+ * https://askubuntu.com/questions/445997/how-to-convert-androids-contacts2-db-to-vcf
---
+++ contacts2vcard.pl
@@ -0,0 +1,84 @@
+#! /usr/bin/perl -w
+
+# Copyright (C) 2020 Thomas More (tmore1@gmx.com)
+# contacts2vcard is free software, released under the terms of the Clarified  Artistic
+# License (1.0), contained in the included file 'LICENSE'
+# contacts2vcard comes with ABSOLUTELY NO WARRANTY
+# contacts2vcard is available at https://github.com/tmo1/contacts2vcard
+# contacts2vcard is documented in its README
+
+# contacts2vcard was designed based on the very helpful (unofficial)
+# documentation of the Android contacts database ('contacts2.db')
+# at https://www.dev2qa.com/android-contacts-database-structure/
+# and https://www.dev2qa.com/android-contacts-fields-data-table-columns-and-data-mimetype-explain/
+
+use Getopt::Std;
+
+use DBI;
+use DBD::SQLite::Constants qw/:extended_result_codes/;
+use vCard;
+
+use strict;
+
+# configuration and setup
+
+my %opts;
+getopts('a:d:', \%opts);
+$opts{'d'} //= "contacts2.db";
+$opts{'a'} //= "address_book.vcf";
+
+my %vcards;
+my %mimetypes = (1 => \&email_v2, 5 => \&phone_v2, 7 => \&name); # taken from the 'mimetypes' table in 'contacts2.db', explained here https://www.dev2qa.com/android-contacts-fields-data-table-columns-and-data-mimetype-explain/ 
+my %email_v2_types = (1 => 'home', 2 => 'work', 3 => 'other', 4 => 'mobile');
+
+# read database
+
+my $dbh = DBI->connect("dbi:SQLite:$opts{'d'}", undef, undef, {RaiseError => 1, PrintError => 0, AutoCommit => 0, sqlite_extended_result_codes => 1});
+my $data = $dbh->selectall_arrayref("SELECT * FROM data");
+
+# process all database rows and create vCards
+
+foreach (@{$data}) {
+	if (exists $mimetypes{${$_}[2]}) {
+		unless (exists $vcards{${$_}[3]}) {$vcards{${$_}[3]} = vCard->new}
+		&{$mimetypes{${$_}[2]}}($_);
+		#print "Row ${$_}[0] successfully processed.\n";
+	}
+}
+
+# create and write address book
+
+my $address_book = vCard::AddressBook->new();
+foreach (values %vcards) {
+	unless (eval {$address_book->load_string($_->as_string)}) {
+		print "vCard processing failed for:\n\n", $_->as_string, "\n$@This may be due to contact information that does not convert to a valid (RFC 6350) vCard.\n";
+	}
+}
+$address_book->as_file($opts{'a'});
+
+# done!
+	
+sub email_v2 {
+	my $row = shift;
+	my $vcard = $vcards{@{$row}[3]};
+	my @previous_addresses;
+	if (my $a = $vcard->email_addresses) {@previous_addresses = @{$a}}
+	$vcard->email_addresses([@previous_addresses, {type => [$email_v2_types{@{$row}[10]}], address => @{$row}[9]}]); # we (currently?) ignore email display name (data4)
+	
+}
+
+sub phone_v2 {
+	my $row = shift;
+	my $vcard = $vcards{@{$row}[3]};
+	my @previous_phones;
+	if (my $p = $vcard->phones) {@previous_phones = @{$p}}
+	$vcard->phones([@previous_phones, {type => [@{$row}[10]], number => @{$row}[9]}]);
+}
+
+sub name {
+	my $row = shift;
+	my $vcard = $vcards{@{$row}[3]};
+	$vcard->full_name(@{$row}[9] // "");
+	$vcard->given_names([@{$row}[10] // ""]);
+	$vcard->family_names([@{$row}[11] // ""]); # we (currently?) ignore prefix, middle name, and suffix (data4, data5, data6)
+}
